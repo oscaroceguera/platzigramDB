d09a36d5a00a6e3574fd929e4838c9b96656a055
oscar <oscaroceguera@gmail.com>
2016-09-29 00:52:29 -0600

    Creacion de un fixture para test de peticion de imagenes

---
+++ test/db-test.js
@@ -0,0 +1,54 @@
+'use strict'
+
+const test = require('ava')
+const uuid = require('uuid-base62')
+const r = require('rethinkdb')
+const Db = require('../')
+const fixtures = require('./fixtures')
+
+const dbName = `platzigram_${uuid.v4()}`
+const db = new Db({ db: dbName })
+
+// HOOKS
+test.before('setup database', async t => {
+  await db.connect()
+  t.true(db.connected, 'should be connected')
+})
+
+test.after('disconnect database', async t => {
+  await db.disconnect()
+  t.false(db.connected, 'should be disconnect')
+})
+
+test.after.always('cleanup database', async t => {
+  let conn = await r.connect({})
+  await r.dbDrop(dbName).run(conn)
+})
+
+test('save image', async t => {
+  t.is(typeof db.saveImage, 'function', 'saveImage is function')
+
+  let image = fixtures.getImage()
+
+  let created = await db.saveImage(image)
+  t.is(created.description, image.description)
+  t.is(created.url, image.url)
+  t.is(created.likes, image.likes)
+  t.is(created.liked, image.liked)
+  t.deepEqual(created.tags, ['awesome', 'tags', 'platzi'])
+  t.is(created.user_id, image.user_id)
+  t.is(typeof created.id, 'string')
+  t.is(created.public_id, uuid.encode(created.id))
+  t.truthy(created.createdAt)
+})
+
+test('like image', async t => {
+  t.is(typeof db.likeImage, 'function', 'likeImage is afunction')
+
+  let image = fixtures.getImage()
+  let created = await db.saveImage(image)
+  let result = await db.likeImage(created.public_id)
+
+  t.true(result.liked)
+  t.is(result.likes, image.likes + 1)
+})
